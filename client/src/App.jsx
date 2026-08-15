5bf45633d3906691ee77e62e094017b205caa87d
Dania Shabih <171351383+MehXFaiz@users.noreply.github.com>
2026-08-15 17:19:01 +0500

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react'
 import PublicLayout from './layouts/PublicLayout'
 import AdminLayout from './layouts/AdminLayout'
 
@@ -20,12 +20,19 @@ import NotFoundPage from './pages/public/NotFoundPage'
 import AdminLoginPage from './pages/admin/AdminLoginPage'
 import AdminDashboardPage from './pages/admin/AdminDashboardPage'
 import AdminProductsPage from './pages/admin/AdminProductsPage'
+import AdminProductFormPage from './pages/admin/AdminProductFormPage'
 import AdminCategoriesPage from './pages/admin/AdminCategoriesPage'
+import AdminCategoryFormPage from './pages/admin/AdminCategoryFormPage'
 import AdminServicesPage from './pages/admin/AdminServicesPage'
+import AdminServiceFormPage from './pages/admin/AdminServiceFormPage'
 import AdminOrdersPage from './pages/admin/AdminOrdersPage'
+import AdminOrderFormPage from './pages/admin/AdminOrderFormPage'
 import AdminQuotesPage from './pages/admin/AdminQuotesPage'
+import AdminQuoteFormPage from './pages/admin/AdminQuoteFormPage'
 import AdminCustomersPage from './pages/admin/AdminCustomersPage'
+import AdminCustomerFormPage from './pages/admin/AdminCustomerFormPage'
 import AdminPortfolioPage from './pages/admin/AdminPortfolioPage'
+import AdminPortfolioFormPage from './pages/admin/AdminPortfolioFormPage'
 import AdminMessagesPage from './pages/admin/AdminMessagesPage'
 import AdminSettingsPage from './pages/admin/AdminSettingsPage'
 
@@ -54,13 +61,43 @@ function App() {
       <Route path="/admin/login" element={<AdminLoginPage />} />
       <Route path="/admin" element={<AdminLayout />}>
         <Route index element={<AdminDashboardPage />} />
-        <Route path="products" element={<AdminProductsPage />} />
+
+        {/* Categories Dedicated Routes */}
         <Route path="categories" element={<AdminCategoriesPage />} />
+        <Route path="categories/new" element={<AdminCategoryFormPage />} />
+        <Route path="categories/:id/edit" element={<AdminCategoryFormPage />} />
+
+        {/* Products Dedicated Routes */}
+        <Route path="products" element={<AdminProductsPage />} />
+        <Route path="products/new" element={<AdminProductFormPage />} />
+        <Route path="products/:id/edit" element={<AdminProductFormPage />} />
+
+        {/* Services Dedicated Routes */}
         <Route path="services" element={<AdminServicesPage />} />
+        <Route path="services/new" element={<AdminServiceFormPage />} />
+        <Route path="services/:id/edit" element={<AdminServiceFormPage />} />
+
+        {/* Orders Dedicated Routes */}
         <Route path="orders" element={<AdminOrdersPage />} />
+        <Route path="orders/new" element={<AdminOrderFormPage />} />
+        <Route path="orders/:id/edit" element={<AdminOrderFormPage />} />
+
+        {/* Quotes Dedicated Routes */}
         <Route path="quotes" element={<AdminQuotesPage />} />
+        <Route path="quotes/new" element={<AdminQuoteFormPage />} />
+        <Route path="quotes/:id/edit" element={<AdminQuoteFormPage />} />
+
+        {/* Customers Dedicated Routes */}
         <Route path="customers" element={<AdminCustomersPage />} />
+        <Route path="customers/new" element={<AdminCustomerFormPage />} />
+        <Route path="customers/:id/edit" element={<AdminCustomerFormPage />} />
+
+        {/* Portfolio Dedicated Routes */}
         <Route path="portfolio" element={<AdminPortfolioPage />} />
+        <Route path="portfolio/new" element={<AdminPortfolioFormPage />} />
+        <Route path="portfolio/:id/edit" element={<AdminPortfolioFormPage />} />
+
+        {/* Messages & Settings */}
         <Route path="messages" element={<AdminMessagesPage />} />
         <Route path="settings" element={<AdminSettingsPage />} />
       </Route>

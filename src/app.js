61cfc4b48eba6dadc93b22bf1e8e5e6ed09bcc85
faizanjumani <faizan2311e@aptechsite.net>
2026-09-11 21:05:14 +0500

    .

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const compression = require('compression')
 const { pool, testConnection } = require('./config/database')
 const { notFound, errorHandler } = require('./middleware/errorHandler')
 const pageSeoService = require('./services/pageSeoService')
+const breadcrumbService = require('./services/breadcrumbService')
 
 const authRoutes = require('./routes/authRoutes')
 const categoryRoutes = require('./routes/categoryRoutes')
